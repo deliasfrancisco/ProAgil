c9c28e3aa00585f8906920eddc2bf2277da038a3
Diego Francisco <diego.francisco@amcom.com.br>
2021-04-26 20:30:52 -0300

    Finalização do crud, flexbox

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -18,12 +18,14 @@ export class EventosComponent implements OnInit {
 
   eventosFiltrados: Evento[];
   eventos: Evento[];
+  evento: Evento;
   imagemLargura  = 50;
   imagemMargem = 2;
   mostrarImagem = false;
   _filtroLista:string = '';
-  modalRef: BsModalRef;
   registerForm: FormGroup;
+  modoSalvar = 'post';
+  bodyDeletarEvento = '';
 
   constructor(
     private eventoService: EventoService,
@@ -33,15 +35,48 @@ export class EventosComponent implements OnInit {
     )
     {
       this.localeService.use('pt-br');
-     }
+      this.registerForm = this.formBuilder.group({
+        tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+        local: ['', Validators.required],
+        dataEvento: ['', Validators.required],
+        qtdPessoas: ['', [Validators.required, Validators.max(1200)]],
+        telefone: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        imagemUrl: ['', Validators.required]
+      });
+    }
 
   ngOnInit() {
-    this.validations();
+    // this.validations();
     this.getEventos();
   }
 
-  salvarAlteracao(){
-
+  salvarAlteracao(template: any){
+  if(this.registerForm.valid){
+    if(this.modoSalvar === 'post'){
+      this.evento = Object.assign({}, this.registerForm.value);
+      this.eventoService.postEvento(this.evento).subscribe(
+        (novoEvento: Evento) => {
+          console.log(novoEvento);
+          template.hide();
+          this.getEventos();
+        }, error => {
+          console.log(error);
+        }
+        );
+      }
+      else{
+        this.evento = Object.assign({id: this.evento.eventoId}, this.registerForm.value);
+        this.eventoService.putEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      }
+    }
   }
 
   public validations(){
@@ -72,8 +107,21 @@ export class EventosComponent implements OnInit {
       );
   }
 
-  openModal(template: TemplateRef<any>){
-    this.modalRef = this.modalService.show(template);
+  editarEvento(evento: Evento, template: any){
+    this.modoSalvar = 'put';
+    this.openModal(template);
+    this.evento = evento;
+    this.registerForm.patchValue(evento);
+  }
+
+  novoEvento(template: any){
+    this.modoSalvar = 'post';
+    this.openModal(template);
+  }
+
+  openModal(template: any){
+    this.registerForm.reset();
+    template.show(template);
   }
 
   alterarImagem(){
@@ -83,12 +131,29 @@ export class EventosComponent implements OnInit {
   getEventos(){
     this.eventoService.getAllEvento().subscribe(
       (_evento: Evento[]) => {
-      this.eventos = _evento;
-      this.eventosFiltrados = this.eventos;
-      console.log(_evento);
-    }, error => {
-      console.log(error);
+        this.eventos = _evento;
+        this.eventosFiltrados = this.eventos;
+        console.log(_evento);
+      }, error => {
+        console.log(error);
 
-    });
+      });
+  }
+
+  excluirEvento(evento: Evento, template: any) {
+    this.openModal(template);
+    this.evento = evento;
+    this.bodyDeletarEvento = `Tem certeza que deseja excluir o Evento: ${evento.tema}, Código: ${evento.tema}`;
+  }
+
+  confirmeDelete(template: any) {
+    this.eventoService.deleteEventoById(this.evento.eventoId).subscribe(
+      () => {
+          template.hide();
+          this.getEventos();
+        }, error => {
+          console.log(error);
+        }
+    );
   }
 }
