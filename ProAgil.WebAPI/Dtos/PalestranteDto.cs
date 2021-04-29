da5db4e9ae4687cae49bdfae56e57052e56c9f57
Diego Francisco <deliasfrancisco@live.com>
2021-04-29 13:15:03 -0300

    Adição das dtos e AutoMapper

---
+++ ProAgil.WebAPI/Dtos/PalestranteDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.WebAPI.Dtos
+{
+    public class PalestranteDto
+    {
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImageUrl { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public List<RedeSocialDto> RedesSociais { get; set; }
+        public List<EventoDto> Eventos { get; set; }
+    }
+}
---
+++ ProAgil.WebAPI/Dtos/RedeSocialDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.WebAPI.Dtos
+{
+    public class RedeSocialDto
+    {
+        public string Nome { get; set; }
+        public string URL { get; set; }
+        public EventoDto Evento { get; set; }
+        public PalestranteDto Palestrante { get; }
+    }
+}
